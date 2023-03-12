34859dde44ee02e7d25f661761eac43ca7687739
Luke <reminderclock@naver.com>
2023-03-12 18:36:12 +0900

    feat:chat1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const axios = require('axios') 
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  let userInput = req.body.userInput
+
+  const url = "https://api.openai.com/v1/completions"
+  const response = await axios.post(url, {
+    model: "text-davinci-003",
+    prompt: userInput,
+    max_tokens: 100,
+    n:1,
+    stop: null,
+    temperature: 0.7,
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${OPENAI_API_KEY}`
+    }
+  })
+  res.status(200).json({ result: response.data.choices[0].text })
 }
---
+++ pages/goodDay.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+const goodDay = () => {
+  const [userInput, setUserInput] = useState('');
+  const [result, setResult] = useState('');
+  useEffect(()=> {
+  }, [userInput])
+  const submit = async () => {
+    const url = "/api/hello"
+    const res = await axios.post(url, {
+      userInput: userInput
+    })
+
+    setResult(res.data.result)
+    return true
+  }
+  return <div className='nice'>
+    <input onChange={e => setUserInput(e.target.value)}></input>
+    <button onClick={e => submit()}>send</button>
+    <div>{result}</div>
+  </div>
+}
+export default goodDay
